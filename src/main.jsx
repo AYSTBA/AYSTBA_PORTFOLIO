e8abcccb26bdd5463507977e0b146c375f0541d7
AYSTBA <AYSTBA-pcll2@qq.com>
2026-07-09 10:06:54 +0800

    Fix: Remove React StrictMode to resolve production rendering issues

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import App from './App.jsx';
 import { LanguageProvider } from './context/LanguageContext';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
-  </StrictMode>
+  <LanguageProvider>
+    <App />
+  </LanguageProvider>
 );
